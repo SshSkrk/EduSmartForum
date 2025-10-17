be5d6cf30fa2b6154e40c857073f33ea59fe3bd9
Oleksandra <sushi05012000gmail.com>
2025-10-17 03:14:52 +0200

    make it better

--- src/main/java/sasha/org/edusmart/EduSmartApplication.java
+++ src/main/java/sasha/org/edusmart/EduSmartApplication.java
@@ -2,9 +2,12 @@ package sasha.org.edusmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = { ReactiveUserDetailsServiceAutoConfiguration.class }
+)
 @EnableCaching
 public class EduSmartApplication {
 
