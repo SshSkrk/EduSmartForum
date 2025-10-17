1ed111709661b64f4a2d217543d588f2e497ddec
Oleksandra <sushi05012000gmail.com>
2025-10-17 02:49:45 +0200

    Update DataSourceConfig with H2 fallback

--- src/main/java/sasha/org/edusmart/config/DataSourceConfig.java
+++ src/main/java/sasha/org/edusmart/config/DataSourceConfig.java
@@ -8,35 +8,47 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 import java.net.URI;
 
+
 @Configuration
 public class DataSourceConfig {
 
     @Value("${DATABASE_URL:}")
-    private String databaseUrl; // empty string if not set
+    private String databaseUrl;
 
     @Bean
-    public DataSource dataSource() throws Exception {
-        if (databaseUrl == null || databaseUrl.isEmpty()) {
-            // Local fallback: H2 database
+    public DataSource dataSource() {
+        try {
+            if (databaseUrl == null || databaseUrl.isEmpty()) {
+                return DataSourceBuilder.create()
+                        .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
+                        .username("sa")
+                        .password("")
+                        .driverClassName("org.h2.Driver")
+                        .build();
+            }
+
+            URI dbUri = new URI(databaseUrl);
+            String[] userInfo = dbUri.getUserInfo().split(":");
+            String username = userInfo.length > 0 ? userInfo[0] : "";
+            String password = userInfo.length > 1 ? userInfo[1] : "";
+            String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost()
+                    + ':' + dbUri.getPort()
+                    + dbUri.getPath()
+                    + "?sslmode=require";
+
             return DataSourceBuilder.create()
-                    .url("jdbc:h2:mem:campusdb")
+                    .url(jdbcUrl)
+                    .username(username)
+                    .password(password)
+                    .driverClassName("org.postgresql.Driver")
+                    .build();
+        } catch (Exception e) {
+            return DataSourceBuilder.create()
+                    .url("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
                     .username("sa")
                     .password("")
                     .driverClassName("org.h2.Driver")
                     .build();
         }
-
-        // Heroku PostgreSQL
-        URI dbUri = new URI(databaseUrl);
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String jdbcUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-
-        return DataSourceBuilder.create()
-                .url(jdbcUrl)
-                .username(username)
-                .password(password)
-                .driverClassName("org.postgresql.Driver")
-                .build();
     }
 }
